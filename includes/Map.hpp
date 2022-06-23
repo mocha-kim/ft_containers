b569aa9e064ff191e6fb503e6180308e43ae80e8
Sunhye <49827929+mocha-kim@users.noreply.github.com>
2022-06-23 22:14:24 +0900

    add ft namespace

--- includes/Map.hpp
+++ includes/Map.hpp
@@ -6,16 +6,18 @@
 /*   By: sunhkim <sunhkim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/23 21:56:08 by sunhkim           #+#    #+#             */
-/*   Updated: 2022/06/23 22:07:01 by sunhkim          ###   ########.fr       */
+/*   Updated: 2022/06/23 22:14:13 by sunhkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MAP_HPP
 #define MAP_HPP
 
+#include "Functional.hpp"
+
 namespace ft
 {
-	template < class Key, class T, class Compare = less<Key>, class Alloc = std::allocator<pair<const Key,T> > >
+	template < class Key, class T, class Compare = ft::less<Key>, class Alloc = std::allocator<pair<const Key,T> > >
 	class map
 	{
 
