a6532b0e3124248babd96564058edd4bdba481d7
Sunhye <49827929+mocha-kim@users.noreply.github.com>
2022-06-23 22:13:07 +0900

    less, binary_function struct

---
+++ includes/Functional.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Functional.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sunhkim <sunhkim@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/23 22:11:48 by sunhkim           #+#    #+#             */
+/*   Updated: 2022/06/23 22:12:19 by sunhkim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MAP_HPP
+#define MAP_HPP
+
+namespace ft
+{
+	template <class Arg1, class Arg2, class Result>
+	struct binary_function {
+		typedef Arg1 first_argument_type;
+		typedef Arg2 second_argument_type;
+		typedef Result result_type;
+	};
+
+	template <class T>
+	struct less : binary_function<T, T, bool>
+	{
+		bool operator() (const T& x, const T& y) const { return (x < y); }
+	};
+}
+
+#endif
