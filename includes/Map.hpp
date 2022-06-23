66ab0659d41fb0559b5aa50199fb8e5836085c70
Sunhye <49827929+mocha-kim@users.noreply.github.com>
2022-06-23 22:02:29 +0900

    init map header

---
+++ includes/Map.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sunhkim <sunhkim@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/23 21:56:08 by sunhkim           #+#    #+#             */
+/*   Updated: 2022/06/23 21:59:42 by sunhkim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MAP_HPP
+#define MAP_HPP
+
+namespace ft
+{
+	template < class Key, class T, class Compare = less<Key>, class Alloc = allocator<pair<const Key,T> > >
+	class map
+	{
+
+	}
+}
+
+#endif
