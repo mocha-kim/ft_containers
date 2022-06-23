953946415313e37e84629cf331baab4518dd6308
Sunhye <49827929+mocha-kim@users.noreply.github.com>
2022-06-23 22:02:49 +0900

    init vector header

---
+++ includes/Vector.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vector.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sunhkim <sunhkim@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/23 21:56:13 by sunhkim           #+#    #+#             */
+/*   Updated: 2022/06/23 21:59:06 by sunhkim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef VECTOR_HPP
+#define VECTOR_HPP
+
+namespace ft
+{
+	template < class T, class Alloc = allocator<T> >
+	class vector
+	{
+		
+	}
+}
+
+#endif
