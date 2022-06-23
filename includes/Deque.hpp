4612004cf5e7ed0fe4eab1a3b57891287b9ffde9
Sunhye <49827929+mocha-kim@users.noreply.github.com>
2022-06-23 22:07:26 +0900

    init deque header

---
+++ includes/Deque.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Deque.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sunhkim <sunhkim@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/23 22:04:57 by sunhkim           #+#    #+#             */
+/*   Updated: 2022/06/23 22:06:58 by sunhkim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DEQUE_HPP
+#define DEQUE_HPP
+
+namespace ft
+{
+	template < class T, class Alloc = std::allocator<T> >
+	class deque
+	{
+		
+	}
+}
+
+#endif
