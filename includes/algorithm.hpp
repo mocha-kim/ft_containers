d46f6e3d67bb9a09d350fbea97db8e69b0ef1aeb
Sunhye <49827929+mocha-kim@users.noreply.github.com>
2022-09-12 14:01:09 +0900

    update code convention

--- includes/algorithm.hpp
+++ includes/algorithm.hpp
@@ -6,7 +6,7 @@
 /*   By: sunhkim <sunhkim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 16:26:08 by sunhkim           #+#    #+#             */
-/*   Updated: 2022/08/05 23:37:43 by sunhkim          ###   ########.fr       */
+/*   Updated: 2022/09/12 14:00:53 by sunhkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,15 +31,16 @@ namespace ft
 	bool lexicographical_compare (InputIterator1 first1, InputIterator1 last1,
 									InputIterator2 first2, InputIterator2 last2)
 	{
-		while (first1!=last1)
+		while (first1 != last1)
 		{
-			if (first2==last2 || *first2<*first1)
+			if (first2 == last2 || *first2 < *first1)
 				return false;
-			else if (*first1<*first2)
+			else if (*first1 < *first2)
 				return true;
-			++first1; ++first2;
+			++first1;
+			++first2;
 		}
-		return (first2!=last2);
+		return first2 != last2;
 	}
 
 	template <typename T>
