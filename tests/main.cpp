3037a505e8ad03b2aa33de800bfc15055388a91e
Sunhye <49827929+mocha-kim@users.noreply.github.com>
2022-09-16 14:12:35 +0900

    update include file

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,24 +1,5 @@
-#include <iostream>
-#include <string>
-#include <deque>
-#if 1 //CREATE A REAL STL EXAMPLE
-	#include <map>
-	#include <stack>
-	#include <vector>
-	namespace ft = std;
-#else
-	#include <map.hpp>
-	#include <stack.hpp>
-	#include <vector.hpp>
-#endif
+#include "test.hpp"
 
-#include "./includes/vector.hpp"
-#include "./includes/stack.hpp"
-#include "./includes/map.hpp"
-#include <stdlib.h>
-
-#define MAX_RAM 4294967296
-#define BUFFER_SIZE 4096
 struct Buffer
 {
 	int idx;
