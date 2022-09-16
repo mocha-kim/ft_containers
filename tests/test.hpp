2ccdfc9ce803ae0103166adc634148e38a1ebe7a
Sunhye <49827929+mocha-kim@users.noreply.github.com>
2022-09-16 17:17:24 +0900

    fix compare print

--- tests/test.hpp
+++ tests/test.hpp
@@ -6,7 +6,7 @@
 /*   By: sunhkim <sunhkim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/16 14:08:50 by sunhkim           #+#    #+#             */
-/*   Updated: 2022/09/16 16:39:38 by sunhkim          ###   ########.fr       */
+/*   Updated: 2022/09/16 17:16:53 by sunhkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,13 +35,22 @@ struct Buffer
 
 #define COUNT (MAX_RAM / (int)sizeof(Buffer))
 
-int ft_test_ft();
-int ft_test_std();
-
-template<class T>
-bool print_value_compare(const T& ft, const T& std)
+template<class T1, class T2>
+bool ite_compare(const T1& ft, const T2& std)
 {
-	bool ret = t1 == t2;
+	if (ft.size() != std.size())
+		return false;
+	int size = ft.size();
+	for (int i = 0; i < size; i++)
+		if (ft[i] != std[i])
+			return false;
+	return true;
+}
+
+template<class T1, class T2>
+bool ite_print_compare(const T1& ft, const T2& std)
+{
+	bool ret = ite_compare(ft, std);
 	if (ret)
 		std::cout << GRN "OK\n" NC;
 	else
@@ -49,12 +58,44 @@ bool print_value_compare(const T& ft, const T& std)
 	return ret;
 }
 
-template<class T>
-bool print_value(const T& ft, const T& std)
+template<class T1, class T2>
+bool st_compare(const T1& ft, const T2& std)
+{
+	if (ft.size() != std.size())
+		return false;
+	int size = ft.size();
+	while(!ft.empty())
+	{
+		if (ft.top() != std.top())
+			return false;
+		ft.pop();
+		std.pop();
+	}
+	return true;
+}
+
+template<class T1, class T2>
+bool st_print_compare(const T1& ft, const T2& std)
+{
+	bool ret = st_compare(ft, std);
+	if (ret)
+		std::cout << GRN "OK\n" NC;
+	else
+		std::cout << RED "KO\n" NC;
+	return ret;
+}
+
+template<class T1, class T2>
+void print_value(const T1& ft, const T2& std)
 {
     std::cout << ">  FT: " << ft << "\n";
     std::cout << "> STD: " << std << "\n";
-    return t1 == t2 ? true : false;
 }
+
+int ft_test_ft();
+int ft_test_std();
+
+int vector_test();
+
 
 #endif
